a17fdacd1a9264642383a8e30022cad4389fc06e
gutierrezandresfelipe <gutierrezandresfelipe@c10c1466-2f8f-11de-bfb9-e98281094dd5>
2009-04-24 02:11:15 +0000

    Agregando definiciones de fuentes

--- Library/Kumbia/Core/ClassPath/CoreClassPath.php
+++ Library/Kumbia/Core/ClassPath/CoreClassPath.php
@@ -18,6 +18,7 @@
  * @copyright	Copyright (c) 2008-2009 Louder Technology COL. (http://www.loudertechnology.com)
  * @copyright	Copyright (c) 2005-2009 Andres Felipe Gutierrez (gutierrezandresfelipe at gmail.com)
  * @license		New BSD License
+ * @version 	$Id$
  */
 
 /**
