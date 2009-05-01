fdf751c4bf242578b2077742d1ef9b9c9b1836b7
gutierrezandresfelipe <gutierrezandresfelipe@c10c1466-2f8f-11de-bfb9-e98281094dd5>
2009-05-01 02:32:05 +0000

    Actualizando version a 1.0.17a

--- trunk/Library/Kumbia/Core/Core.php
+++ trunk/Library/Kumbia/Core/Core.php
@@ -39,7 +39,7 @@ abstract class Core {
 	 * Version del Framework
 	 *
 	 */
-	const FRAMEWORK_VERSION = '1.0.17GA';
+	const FRAMEWORK_VERSION = '1.0.17aGA';
 
 	/**
 	 * PATH donde esta instalada la instancia del framework
