09978718b5a54061122475ef3d31dc5f1a671a93
game013 <game013@c10c1466-2f8f-11de-bfb9-e98281094dd5>
2009-11-09 15:30:28 +0000

    Agregando cambios en Tag: implementada una función que genera campos con formato porcentaje; corregido un bug en el selectStatic

--- Library/Kumbia/Tag/Tag.php
+++ Library/Kumbia/Tag/Tag.php
@@ -456,6 +456,77 @@ abstract class Tag {
 	}
 
 	/**
+	 * Crea una caja de texto que solo acepta numeros y los formatea como porcentaje
+	 *
+	 * @param 	mixed $params
+	 * @return 	string
+	 * @static
+	 */
+	public static function percentField($params){
+		$numberArguments = func_num_args();
+		$params = Utils::getParams(func_get_args(), $numberArguments);
+		if(!isset($params[0])){
+			$params[0] = $params['id'];
+		}
+		if(!isset($params['name'])||!$params['name']){
+			$params['name'] = $params[0];
+		}
+		if(isset($params['value'])){
+			$value = $params['value'];
+			unset($params['value']);
+		} else {
+			$value = self::getValueFromAction($params[0]);
+		}
+		if(!isset($params['onkeydown'])) {
+			$params['onkeydown'] = "valNumeric(event)";
+		} else {
+			$params['onkeydown'].=";valNumeric(event)";
+		}
+        if(!isset($params['formatOptions'])){
+            $params['formatOptions'] = '';
+        }
+        if(isset($params['objectFormat'])){
+		    if(!isset($params['onblur'])) {
+			    $params['onblur'] = "this.value={$params['objectFormat']}.percent(this.value);";
+		    } else {
+			    $params['onblur'].= ";this.value={$params['objectFormat']}.percent(this.value);";
+		    }
+		    if(!isset($params['onfocus'])) {
+			    $params['onfocus'] = "this.value={$params['objectFormat']}.deFormat(this.value,\"percent\");this.activate();";
+		    } else {
+			    $params['onfocus'].= ";this.value={$params['objectFormat']}.deFormat(this.value,\"percent\");this.activate();";
+		    }
+            $codeAlt = "<script type='text/javascript'>\n\t$('{$params[0]}').value={$params['objectFormat']}.percent($('{$params[0]}').value);\n</script>\r\n";
+            unset($params['objectFormat']);
+        }else{
+		    if(!isset($params['onblur'])) {
+			    $params['onblur'] = "defaultFormater=new Format({$params['formatOptions']});this.value=defaultFormater.percent(this.value);";
+		    } else {
+			    $params['onblur'].=";defaultFormater=new Format({$params['formatOptions']});this.value=defaultFormater.percent(this.value);";
+		    }
+		    if(!isset($params['onfocus'])) {
+			    $params['onfocus'] = "defaultFormater=new Format({$params['formatOptions']});this.value=defaultFormater.deFormat(this.value,\"percent\");this.activate();";
+		    } else {
+			    $params['onfocus'].= ";defaultFormater=new Format({$params['formatOptions']});this.value=defaultFormater.deFormat(this.value,\"percent\");this.activate();";
+		    }
+            $codeAlt = "<script type='text/javascript'>\n\tdefaultFormater=new Format({$params['formatOptions']});\n\t$('{$params[0]}').value=defaultFormater.percent($('{$params[0]}').value);\n</script>\r\n";
+        }
+        unset($params['formatOptions']);
+        $params['format'] = 'percent';
+		$code = "<input type='text' id='{$params[0]}' value='$value' ";
+		foreach($params as $key => $val){
+			if(!is_integer($key)){
+				$code.="$key='$val' ";
+			}
+		}
+		$code.=" />\r\n";
+        if(isset($codeAlt) && (!empty($value) || $value == 0)){
+            $code.= $codeAlt;
+        }
+		return $code;
+	}
+
+	/**
 	 * Crea una caja de password que solo acepta numeros
 	 *
 	 * @param 	mixed $params
@@ -777,7 +848,7 @@ abstract class Tag {
 			}
 			if(is_array($params[1])){
 				foreach($params[1] as $k => $d){
-					if($k==$value){
+					if($k==$value && $value!==''){
 						$code.="\t<option value='$k' selected='selected'>$d</option>\r\n";
 					} else {
 						$code.="\t<option value='$k'>$d</option>\r\n";
